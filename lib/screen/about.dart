933fbf3771752aa047203eae7f9db7b41ad776f7
samuelhalim12 <samuelhalim12@gmail.com>
2023-01-05 13:44:27 +0700

    test tambah komen

--- lib/screen/about.dart
+++ lib/screen/about.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+//coba tambah komen
 class About extends StatelessWidget {
   List<Widget> cats() {
     List<Widget> temp = [];
